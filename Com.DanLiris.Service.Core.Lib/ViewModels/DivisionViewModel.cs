fcacc81d3089549afaf4a93712b7135854cefd4b
adityahenanda <henanda.aditya@gmail.com>
2018-07-16 14:45:49 +0700

    Revert "add marter services"
    This reverts commit 14bbf2707514fa0915066e671eae01542b7984c4.

--- Com.DanLiris.Service.Core.Lib/ViewModels/DivisionViewModel.cs
+++ Com.DanLiris.Service.Core.Lib/ViewModels/DivisionViewModel.cs
@@ -1,14 +1,31 @@
-﻿using Com.DanLiris.Service.Core.Lib.Helpers;
-using System;
+﻿using System;
 
 namespace Com.DanLiris.Service.Core.Lib.ViewModels
 {
-    public class DivisionViewModel : BasicViewModel
+    public class DivisionViewModel
     {
-        public string Code { get; set; }
+        public int _id { get; set; }
 
-        public string Name { get; set; }
+        public bool _deleted { get; set; }
 
-        public string Description { get; set; }
+        public bool _active { get; set; }
+
+        public DateTime _createdDate { get; set; }
+
+        public string _createdBy { get; set; }
+
+        public string _createAgent { get; set; }
+
+        public DateTime _updatedDate { get; set; }
+
+        public string _updatedBy { get; set; }
+
+        public string _updateAgent { get; set; }
+
+        public string code { get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; }
     }
 }
--- Com.DanLiris.Service.Core.Lib/ViewModels/UnitViewModel.cs
+++ Com.DanLiris.Service.Core.Lib/ViewModels/UnitViewModel.cs
@@ -1,16 +1,42 @@
-﻿using Com.DanLiris.Service.Core.Lib.Helpers;
-using System;
+﻿using System;
 
 namespace Com.DanLiris.Service.Core.Lib.ViewModels
 {
-    public class UnitViewModel  : BasicViewModel
+    public class UnitViewModel
     {
-        public string Code { get; set; }
+        public int _id { get; set; }
 
-        public DivisionViewModel Division { get; set; }
+        public bool _deleted { get; set; }
 
-        public string Name { get; set; }
+        public bool _active { get; set; }
 
-        public string Description { get; set; }
+        public DateTime _createdDate { get; set; }
+
+        public string _createdBy { get; set; }
+
+        public string _createAgent { get; set; }
+
+        public DateTime _updatedDate { get; set; }
+
+        public string _updatedBy { get; set; }
+
+        public string _updateAgent { get; set; }
+
+        public string code { get; set; }
+
+        public UnitDivisionViewModel division { get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; }
+    }
+
+    public class UnitDivisionViewModel
+    {
+        public int _id { get; set; }
+
+        public string code { get; set; }
+
+        public string name { get; set; }
     }
 }
