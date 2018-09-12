73a4683d0866ff00f9d03e16ed00da7c59555310
achensetyawati <achenzetya@gmail.com>
2018-09-12 15:38:24 +0700

    Garment Category Migration

--- Com.DanLiris.Service.Core.Lib/Migrations/20180912074935_initialGarmentCategories.Designer.cs
+++ Com.DanLiris.Service.Core.Lib/Migrations/20180912074935_initialGarmentCategories.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Com.DanLiris.Service.Core.Lib.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180912074935_initialGarmentCategories")]
+    partial class initialGarmentCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
