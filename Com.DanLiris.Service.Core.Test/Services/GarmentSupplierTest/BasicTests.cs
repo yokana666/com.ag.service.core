45c32c8c0c703bf45285494f54f14f8feade75a7
widiartowahyu3 <43228909+widiartowahyu3@users.noreply.github.com>
2018-10-18 15:47:59 +0700

    fix unit test income tax pph

--- Com.DanLiris.Service.Core.Test/Services/GarmentSupplierTest/BasicTests.cs
+++ Com.DanLiris.Service.Core.Test/Services/GarmentSupplierTest/BasicTests.cs
@@ -12,30 +12,24 @@ namespace Com.DanLiris.Service.Core.Test.Services.GarmentSupplierTest
 	[Collection("ServiceProviderFixture Collection")]
 	public class BasicTests : BasicServiceTest<CoreDbContext, GarmentSupplierService, GarmentSupplier>
 	{
-		private static readonly string[] createAttrAssertions = { "Name" };
-		private static readonly string[] updateAttrAssertions = { "Name" };
+		private static readonly string[] createAttrAssertions = { "Name","IncomeTaxesRate" };
+		private static readonly string[] updateAttrAssertions = { "Name","IncomeTaxesRate" };
 		private static readonly string[] existAttrCriteria = { "Code" };
 		public BasicTests(ServiceProviderFixture fixture) : base(fixture, createAttrAssertions, updateAttrAssertions, existAttrCriteria)
 		{
 		}
 		public override void EmptyCreateModel(GarmentSupplier model)
 		{
-			model.Code = string.Empty;
-			model.Name = string.Empty;
+			//model.Code = string.Empty;
+			//model.Name = string.Empty;
 			model.UseTax = true;
-            model.IncomeTaxesId = 1;
-			model.IncomeTaxesName = string.Empty;
-            model.IncomeTaxesRate = null;
 		}
 
 		public override void EmptyUpdateModel(GarmentSupplier model)
 		{
-			model.Code = string.Empty;
-			model.Name = string.Empty;
+			//model.Code = string.Empty;
+			//model.Name = string.Empty;
 			model.UseTax = true;
-            model.IncomeTaxesId = 1;
-			model.IncomeTaxesName = string.Empty;
-            model.IncomeTaxesRate = null;
 		}
 		public override GarmentSupplier GenerateTestModel()
 		{
