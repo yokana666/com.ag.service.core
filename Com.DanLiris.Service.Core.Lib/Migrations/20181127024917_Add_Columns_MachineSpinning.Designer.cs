acf4bf8f3a6785cfccf3ac758cf5ee4e55a3ecaf
achensetyawati <achenzetya@gmail.com>
2018-11-27 14:07:41 +0700

    spinning machine migrations

--- Com.DanLiris.Service.Core.Lib/Migrations/20181127024917_Add_Columns_MachineSpinning.Designer.cs
+++ Com.DanLiris.Service.Core.Lib/Migrations/20181127024917_Add_Columns_MachineSpinning.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Com.DanLiris.Service.Core.Lib.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181127024917_Add_Columns_MachineSpinning")]
+    partial class Add_Columns_MachineSpinning
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Com.DanLiris.Service.Core.Lib/Migrations/20181127024917_Add_Columns_MachineSpinning.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Core.Lib.Migrations
+{
+    public partial class Add_Columns_MachineSpinning : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "MachineSpinnings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CounterCondition",
+                table: "MachineSpinnings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UomId",
+                table: "MachineSpinnings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UomUnit",
+                table: "MachineSpinnings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "MachineSpinnings");
+
+            migrationBuilder.DropColumn(
+                name: "CounterCondition",
+                table: "MachineSpinnings");
+
+            migrationBuilder.DropColumn(
+                name: "UomId",
+                table: "MachineSpinnings");
+
+            migrationBuilder.DropColumn(
+                name: "UomUnit",
+                table: "MachineSpinnings");
+        }
+    }
+}
