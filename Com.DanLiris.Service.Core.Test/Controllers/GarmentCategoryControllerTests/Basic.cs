00262774b2f82b62dbef537b00822583683758d9
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-12-15 09:56:49 +0700

    Update Basic.cs

--- Com.DanLiris.Service.Core.Test/Controllers/GarmentCategoryControllerTests/Basic.cs
+++ Com.DanLiris.Service.Core.Test/Controllers/GarmentCategoryControllerTests/Basic.cs
@@ -1,9 +1,4 @@
-﻿using Com.DanLiris.Service.Core.Lib;
-using Com.DanLiris.Service.Core.Lib.Models;
-using Com.DanLiris.Service.Core.Lib.Services;
-using Com.DanLiris.Service.Core.Lib.ViewModels;
-using Com.DanLiris.Service.Core.Test.DataUtils;
-using Com.DanLiris.Service.Core.Test.Helpers;
+﻿using Com.DanLiris.Service.Core.Lib.ViewModels;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -16,44 +11,39 @@ using Xunit;
 namespace Com.DanLiris.Service.Core.Test.Controllers.GarmentCategoryControllerTests
 {
     [Collection("TestFixture Collection")]
-    public class Basic : BasicControllerTest<CoreDbContext, GarmentCategoryService, GarmentCategory, GarmentCategoryViewModel, GarmentCategoryDataUtil>
+    public class Basic
     {
         private const string URI = "v1/master/garment-categories";
-        private static List<string> CreateValidationAttributes = new List<string> { };
-        private static List<string> UpdateValidationAttributes = new List<string> { };
 
-        //protected TestServerFixture TestFixture { get; set; }
+        protected TestServerFixture TestFixture { get; set; }
 
-        //protected HttpClient Client
-        //{
-        //    get { return this.TestFixture.Client; }
-        //}
-
-        public Basic(TestServerFixture fixture) : base(fixture, URI, CreateValidationAttributes, UpdateValidationAttributes)
+        protected HttpClient Client
         {
+            get { return this.TestFixture.Client; }
         }
 
-        protected GarmentCategoryDataUtil DataUtil
+        public Basic(TestServerFixture fixture)
         {
-            get { return (GarmentCategoryDataUtil)this.TestFixture.Service.GetService(typeof(GarmentCategoryDataUtil)); }
+            TestFixture = fixture;
+
         }
 
-        //public GarmentCategoryViewModel GenerateTestModel()
-        //{
-        //    string guid = Guid.NewGuid().ToString();
+        public GarmentCategoryViewModel GenerateTestModel()
+        {
+            string guid = Guid.NewGuid().ToString();
 
-        //    return new GarmentCategoryViewModel()
-        //    {
-        //        code = guid,
-        //        name = string.Format("TEST g-category {0}", guid),
-        //        codeRequirement = string.Format("TEST g-category {0}", guid),
-        //        uom=new UomViewModel
-        //        {
-        //            Id=1,
-        //            Unit = string.Format("TEST g-category {0}", guid)
-        //        }
-        //    };
-        //}
+            return new GarmentCategoryViewModel()
+            {
+                code = guid,
+                name = string.Format("TEST g-category {0}", guid),
+                codeRequirement = string.Format("TEST g-category {0}", guid),
+                uom=new UomViewModel
+                {
+                    Id=1,
+                    Unit = string.Format("TEST g-category {0}", guid)
+                }
+            };
+        }
 
         //[Fact]
         //public async Task Get()
@@ -69,33 +59,14 @@ namespace Com.DanLiris.Service.Core.Test.Controllers.GarmentCategoryControllerTe
         //    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         //}
 
-        //[Fact]
-        //public async Task Post()
-        //{
-
-        //    GarmentCategoryViewModel categoryViewModel = GenerateTestModel();
-        //    var response = await this.Client.PostAsync(URI, new StringContent(JsonConvert.SerializeObject(categoryViewModel).ToString(), Encoding.UTF8, "application/json"));
-
-        //    Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-        //}
-
         [Fact]
-        public async Task Should_Success_Get_Data_By_Code()
+        public async Task Post()
         {
-            string byCodeUri = "v1/master/garment-categories/byCode";
-            GarmentCategory Model = await DataUtil.GetTestDataAsync();
-            GarmentCategoryViewModel ViewModel = Service.MapToViewModel(Model);
 
-            var response = await this.Client.GetAsync(string.Concat(byCodeUri, "/", ViewModel.code));
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            GarmentCategoryViewModel categoryViewModel = GenerateTestModel();
+            var response = await this.Client.PostAsync(URI, new StringContent(JsonConvert.SerializeObject(categoryViewModel).ToString(), Encoding.UTF8, "application/json"));
 
-            var json = response.Content.ReadAsStringAsync().Result;
-            Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(json.ToString());
-
-            Assert.True(result.ContainsKey("apiVersion"));
-            Assert.True(result.ContainsKey("message"));
-            Assert.True(result.ContainsKey("data"));
-            Assert.True(result["data"].GetType().Name.Equals("JObject"));
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
     }
 }
