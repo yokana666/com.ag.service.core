743fb14e0d283cb227737a7ad8354815a4a57afb
leslieaula <cristiano.ulo@gmail.com>
2019-01-04 11:05:20 +0700

    Update Docker Command And Coverage Ignore

--- Com.Danliris.Migration.Mongo.Model.Lib/MongoDbSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Com.Danliris.Migration.Mongo.Model.Lib
-{
-    public class MongoDbSettings
-    {
-        public string ConnectionString { get; set; }
-        public string Database { get; set; }
-    }
-}
