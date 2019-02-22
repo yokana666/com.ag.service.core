32f048443535bb592f56300954865403bcfc471c
Ali Reza <ali.syariati@moonlay.com>
2019-02-22 19:33:10 +0700

    add migration

--- Com.DanLiris.Service.Core.Lib/Migrations/20190222123207_FixLengthDescInProduct.Designer.cs
+++ Com.DanLiris.Service.Core.Lib/Migrations/20190222123207_FixLengthDescInProduct.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Com.DanLiris.Service.Core.Lib.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190222123207_FixLengthDescInProduct")]
+    partial class FixLengthDescInProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
