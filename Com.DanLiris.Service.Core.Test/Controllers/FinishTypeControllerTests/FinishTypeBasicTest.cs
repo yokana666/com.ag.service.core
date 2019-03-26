b089497995db5d8d9d162531922630f4cbfbd271
kuswandanu <kuswandanu@gmail.com>
2019-03-26 08:55:54 +0700

    FinishType and ColorType Test

---
+++ Com.DanLiris.Service.Core.Test/Controllers/FinishTypeControllerTests/FinishTypeBasicTest.cs
@@ -0,0 +1,23 @@
+﻿using Com.DanLiris.Service.Core.Lib;
+using Com.DanLiris.Service.Core.Lib.Models;
+using Com.DanLiris.Service.Core.Lib.Services;
+using Com.DanLiris.Service.Core.Lib.ViewModels;
+using Com.DanLiris.Service.Core.Test.DataUtils;
+using Com.DanLiris.Service.Core.Test.Helpers;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Com.DanLiris.Service.Core.Test.Controllers.FinishTypeControllerTests
+{
+    [Collection("TestFixture Collection")]
+    public class FinishTypeBasicTest : BasicControllerTest<CoreDbContext, FinishTypeService, FinishType, FinishTypeViewModel, FinishTypeDataUtil>
+    {
+        private static string URI = "v1/master/finish-types";
+        private static List<string> CreateValidationAttributes = new List<string> { };
+        private static List<string> UpdateValidationAttributes = new List<string> { "Name", "Code" };
+
+        public FinishTypeBasicTest(TestServerFixture fixture) : base(fixture, URI, CreateValidationAttributes, UpdateValidationAttributes)
+        {
+        }
+    }
+}
---
+++ Com.DanLiris.Service.Core.Test/DataUtils/FinishTypeDataUtil.cs
@@ -0,0 +1,43 @@
+﻿using Com.DanLiris.Service.Core.Lib;
+using Com.DanLiris.Service.Core.Lib.Models;
+using Com.DanLiris.Service.Core.Lib.Services;
+using Com.DanLiris.Service.Core.Lib.ViewModels;
+using Com.DanLiris.Service.Core.Test.Helpers;
+using Com.DanLiris.Service.Core.Test.Interface;
+using System;
+using System.Threading.Tasks;
+
+namespace Com.DanLiris.Service.Core.Test.DataUtils
+{
+    public class FinishTypeDataUtil : BasicDataUtil<CoreDbContext, FinishTypeService, FinishType>, IEmptyData<FinishTypeViewModel>
+    {
+        public FinishTypeDataUtil(CoreDbContext dbContext, FinishTypeService service) : base(dbContext, service)
+        {
+        }
+
+        public FinishTypeViewModel GetEmptyData()
+        {
+            FinishTypeViewModel ViewModel = new FinishTypeViewModel();
+            return ViewModel;
+        }
+
+        public override FinishType GetNewData()
+        {
+            Guid guid = Guid.NewGuid();
+            FinishType model = new FinishType()
+            {
+                Name = "FinishTypesName" + guid,
+                Code = "FinishTypesCode" + guid,
+                Remark = "FinishTypesRemark" + guid,
+            };
+            return model;
+        }
+
+        public override async Task<FinishType> GetTestDataAsync()
+        {
+            FinishType model = GetNewData();
+            await Service.CreateModel(model);
+            return model;
+        }
+    }
+}
