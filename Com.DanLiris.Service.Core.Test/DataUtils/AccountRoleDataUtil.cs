84397585fca4bad57e3f03e64cee59fcab0cc080
kuswandanu <kuswandanu@gmail.com>
2019-03-26 09:17:21 +0700

    Salah nyelect :smiley:

---
+++ Com.DanLiris.Service.Core.Test/DataUtils/AccountRoleDataUtil.cs
@@ -0,0 +1,36 @@
+﻿using Com.DanLiris.Service.Core.Lib;
+using Com.DanLiris.Service.Core.Lib.Models.Account_and_Roles;
+using Com.DanLiris.Service.Core.Lib.Services.Account_and_Roles;
+using Com.DanLiris.Service.Core.Lib.ViewModels.Account_and_Roles;
+using Com.DanLiris.Service.Core.Test.Helpers;
+using Com.DanLiris.Service.Core.Test.Interface;
+using System.Threading.Tasks;
+
+namespace Com.DanLiris.Service.Core.Test.DataUtils
+{
+    public class AccountRoleDataUtil : BasicDataUtil<CoreDbContext, AccountRoleService, AccountRole>, IEmptyData<AccountRoleViewModel>
+    {
+        public AccountRoleDataUtil(CoreDbContext dbContext, AccountRoleService service) : base(dbContext, service)
+        {
+        }
+
+        public AccountRoleViewModel GetEmptyData()
+        {
+            return new AccountRoleViewModel();
+        }
+
+        public override AccountRole GetNewData()
+        {
+            return new AccountRole{
+                
+            };
+        }
+
+        public override async Task<AccountRole> GetTestDataAsync()
+        {
+            var model = GetNewData();
+            await Service.CreateModel(model);
+            return model;
+        }
+    }
+}
---
+++ Com.DanLiris.Service.Core.Test/DataUtils/PermissionDataUtil.cs
@@ -0,0 +1,36 @@
+﻿using Com.DanLiris.Service.Core.Lib;
+using Com.DanLiris.Service.Core.Lib.Models.Account_and_Roles;
+using Com.DanLiris.Service.Core.Lib.Services.Account_and_Roles;
+using Com.DanLiris.Service.Core.Lib.ViewModels.Account_and_Roles;
+using Com.DanLiris.Service.Core.Test.Helpers;
+using Com.DanLiris.Service.Core.Test.Interface;
+using System.Threading.Tasks;
+
+namespace Com.DanLiris.Service.Core.Test.DataUtils
+{
+    public class PermissionDataUtil : BasicDataUtil<CoreDbContext, PermissionService, Permission>, IEmptyData<PermissionViewModel>
+    {
+        public PermissionDataUtil(CoreDbContext dbContext, PermissionService service) : base(dbContext, service)
+        {
+        }
+
+        public PermissionViewModel GetEmptyData()
+        {
+            return new PermissionViewModel();
+        }
+
+        public override Permission GetNewData()
+        {
+            return new Permission{
+                
+            };
+        }
+
+        public override async Task<Permission> GetTestDataAsync()
+        {
+            var model = GetNewData();
+            await Service.CreateModel(model);
+            return model;
+        }
+    }
+}
