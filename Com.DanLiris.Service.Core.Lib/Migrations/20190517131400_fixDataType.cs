1bdb01b9641500182dd627e1cd04cf4774ec6b4c
Ali Reza <ali.syariati@moonlay.com>
2019-05-17 20:53:56 +0700

    fix type data product

---
+++ Com.DanLiris.Service.Core.Lib/Migrations/20190517131400_fixDataType.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Core.Lib.Migrations
+{
+    public partial class fixDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Weight",
+                table: "ProductSPPProperties",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Length",
+                table: "ProductSPPProperties",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Weight",
+                table: "ProductSPPProperties",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Length",
+                table: "ProductSPPProperties",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
