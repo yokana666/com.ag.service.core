e08f612abd3e6e5c41e4d427e051f8cbb68fc72c
sama08 <usamah_alan@hotmail.com>
2019-12-20 14:08:29 +0700

    Create DesignMotiveBasicTest.cs

---
+++ Com.DanLiris.Service.Core.Test/Services/DesignMotiveTest/DesignMotiveBasicTest.cs
@@ -0,0 +1,45 @@
+﻿using Com.Danliris.Service.Core.Test.Helpers;
+using Com.DanLiris.Service.Core.Lib;
+using Com.DanLiris.Service.Core.Lib.Models;
+using Com.DanLiris.Service.Core.Lib.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.DanLiris.Service.Core.Test.Services.DesignMotiveTest
+{
+    [Collection("ServiceProviderFixture Collection")]
+    public class DesignMotiveBasicTest : BasicServiceTest<CoreDbContext, DesignMotiveService, DesignMotive>
+    {
+        private static readonly string[] createAttrAssertions = { "Name" };
+        private static readonly string[] updateAttrAssertions = { "Name" };
+        private static readonly string[] existAttrCriteria = { "Name" };
+
+        public DesignMotiveBasicTest(ServiceProviderFixture fixture, string[] createAttrAssertions, string[] updateAttrAssertions, string[] existAttrCriteria) : base(fixture, createAttrAssertions, updateAttrAssertions, existAttrCriteria)
+        {
+        }
+
+        public override void EmptyCreateModel(DesignMotive model)
+        {
+            model.Code = string.Empty;
+            model.Name = string.Empty;
+        }
+
+        public override void EmptyUpdateModel(DesignMotive model)
+        {
+            model.Code = string.Empty;
+            model.Name = string.Empty;
+        }
+
+        public override DesignMotive GenerateTestModel()
+        {
+            string guid = Guid.NewGuid().ToString();
+
+            return new DesignMotive()
+            {
+                Name = String.Concat("TEST MOTIVE DESIGN", guid),
+            };
+        }
+    }
+}
