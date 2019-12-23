84236d40e7ddfc4b17d171c06cf6c8a77781c679
sama08 <usamah_alan@hotmail.com>
2019-12-23 08:16:49 +0700

    Holiday Test

--- Com.DanLiris.Service.Core.Test/ServiceProviderFixture.cs
+++ Com.DanLiris.Service.Core.Test/ServiceProviderFixture.cs
@@ -34,6 +34,7 @@ namespace Com.DanLiris.Service.Core.Test
                 {
                     options.UseSqlServer(connectionString);
                 }, ServiceLifetime.Transient)
+                .AddTransient<HolidayService>(provider => new HolidayService(provider))
                 .AddTransient<DesignMotiveService>(provider => new DesignMotiveService(provider))
                 .AddTransient<BudgetService>(provider => new BudgetService(provider))
                 .AddTransient<BudgetServiceDataUtil>()
